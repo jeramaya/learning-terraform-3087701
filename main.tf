47c78985cf7a2f8d7dafbd4727b00e5879def39b
jeramaya <53906069+jeramaya@users.noreply.github.com>
2024-07-10 14:06:37 -0400

    use module for security group

--- main.tf
+++ main.tf
@@ -18,10 +18,12 @@ data "aws_vpc" "default"{
 default = true
 }
 
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+vpc_security_group_ids = [module.blog_sg.security_group_id]
+
 
   vpc_security_group_ids = [aws_security_group.blog.id]
 
@@ -30,9 +32,20 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+name = "new_blog"
 
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-resource "aws_security_group" "blog" {
+  egress_rules        = ["all-all"]
+  egress_cidir_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "blog" { 
  name           = "blog"
 description     = "Allow http and https in. Allow everything out"
 
