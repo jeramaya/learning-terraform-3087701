45ffab9ff2fd49319b87527b6405a2447a8c901c
jeramaya <53906069+jeramaya@users.noreply.github.com>
2024-07-10 14:07:54 -0400

    use module for security group

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "blog" {
 vpc_security_group_ids = [module.blog_sg.security_group_id]
 
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+
 
   tags = {
     Name = "Learing Terraform"
